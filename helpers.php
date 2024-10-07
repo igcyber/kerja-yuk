7288762a46604e643edc08033745bb9e0116b6fa
Aji Bagaskara <ajibagaskara08@gmail.com>
2024-10-07 20:52:48 +0800

    Split UI into partials|Create loadView & loadPartial helper function

--- helpers.php
+++ helpers.php
@@ -9,3 +9,39 @@
  function basePath($path = ''){
     return __DIR__. '/' . $path;
  }
+
+
+ /**
+  * @param string $name
+  * 
+  * @return void
+  */
+ function loadView($name)
+ {
+    $viewPath = basePath("views/{$name}.php");
+
+    if(file_exists($viewPath))
+    {
+        require $viewPath;
+    }else{
+        echo "View {$viewPath} not found";
+    }
+ }
+
+
+ /**
+  * @param string $name
+  * 
+  * @return void
+  */
+ function loadPartial($name)
+ {
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if(file_exists($partialPath))
+    {
+        require $partialPath;
+    }else{
+        echo "View {$partialPath} not found";
+    }
+ }
---
+++ views/partials/footer.php
@@ -0,0 +1,37 @@
+<div class="site-footer">
+    <div class="row">
+        <div class="col-12 text-center">
+        <p>
+            Copyright &copy;
+            <script>
+            document.write(new Date().getFullYear());
+            </script>
+            . All Rights Reserved. &mdash; Designed with love by
+            <a href="https://untree.co">Untree.co</a>
+            <!-- License information: https://untree.co/license/ -->
+        </p>
+            <div>
+                Distributed by
+                <a href="https://themewagon.com/" target="_blank">themewagon</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    <!-- /.container -->
+</div>
+<!-- /.site-footer -->
+
+<!-- Preloader -->
+<div id="overlayer"></div>
+<div class="loader">
+    <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+    </div>
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/tiny-slider.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/navbar.js"></script>
+<script src="js/counter.js"></script>
+<script src="js/custom.js"></script>
